e0715d5febf23187146c9a06ec5418206a45edb8
fridman32 <yehonatan.fridman@gmail.com>
2021-08-31 12:25:21 +0300

    base server

--- server/index.js
+++ server/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
 import postRoutes from './routes/posts.js'
 
 const app = express();
 
+app.use('/posts', postRoutes);
 //set size limits for images
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
