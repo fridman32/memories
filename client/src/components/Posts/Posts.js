a9c220434bbe1b15a77d8547e0b35eb437ad7a19
fridman32 <yehonatan.fridman@gmail.com>
2021-08-31 13:06:42 +0300

    client structure

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Post from './post/Post'
+
+function Posts() {
+    return (
+        <>
+            <h1>Posts</h1>
+            <Post/>
+            <Post/>
+        </>
+    )
+}
+
+export default Posts
